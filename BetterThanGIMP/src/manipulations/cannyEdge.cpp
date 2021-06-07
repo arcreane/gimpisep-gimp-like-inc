15756a80db4a643eec6d6b1e6ca128670d268bd9
Hugues Baratgin <hugues.baratgin@gmail.com>
2021-06-07 16:01:48 +0200

    Fonction cannyEdge

--- BetterThanGIMP/src/manipulations/cannyEdge.cpp
+++ BetterThanGIMP/src/manipulations/cannyEdge.cpp
@@ -1,4 +1,10 @@
-//
-// Created by yakiimo on 6/7/21.
-//
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
+
+Mat cannyEdge(Mat image, int threshold1, int threshold2, int kernelSize) {
+    cvtColor(image, image, COLOR_BGR2GRAY);
+    GaussianBlur(image, image, Size(kernelSize, kernelSize), 0);
+    Canny(image, image, threshold1, threshold2, kernelSize, false);
+    return image;
+}
