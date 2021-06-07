81464ec02e1ae7e3f84914c7fd2fe5fcd037efa7
Sean <tang-sean.lim@isep.fr>
2021-06-07 16:09:43 +0200

    Erosion and Dilatation

--- BetterThanGIMP/src/manipulations/erosion.cpp
+++ BetterThanGIMP/src/manipulations/erosion.cpp
@@ -1,4 +1,9 @@
 //
 // Created by yakiimo on 6/7/21.
 //
+cv::Mat erosion(cv::Mat imageInput, cv::Mat kernel, int iteration){
+    cv :: Mat imageDestination;
+    cv::erode(imageInput,imageDestination,kernel,cv::Point(-1,-1) ,iteration);
 
+    return imageDestination;
+}
