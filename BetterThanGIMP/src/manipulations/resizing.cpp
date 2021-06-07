65687b7f0022f79f7a6a8176d3a6136d02ff9a53
LouLou-ISEP <58336500+LouLou-ISEP@users.noreply.github.com>
2021-06-07 17:15:04 +0200

    Update resizing.cpp

--- BetterThanGIMP/src/manipulations/resizing.cpp
+++ BetterThanGIMP/src/manipulations/resizing.cpp
@@ -1,4 +1,48 @@
 //
 // Created by yakiimo on 6/7/21.
 //
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+using namespace cv;
+using namespace std;
+
+/* Option 0 for resizing with dimensions, option 1 for resizing with a factor */
+cv::Mat resizing(Mat input, Mat output,double scaleX, double scaleY, int option){
+	double input_height = input.size().height;
+	double input_width = input.size().width;
+
+	if (option == 0) {
+		/* Resize down*/
+		if (input_height * input_width < scaleX * scaleY) {
+			resize(input, output, Size(scaleX, scaleY), INTER_AREA);
+		}
+		/* Resize up*/
+		else if (input_height * input_width > scaleX * scaleY) {
+			resize(input, output, Size(scaleX, scaleY), INTER_CUBIC);
+		}
+		/* Same size*/
+		else {
+			resize(input, output, Size(scaleX, scaleY), INTER_LINEAR);
+		}
+	}
+	else if (option == 1) {
+		/* Rescale down*/
+		if (input_height * input_width < scaleX * scaleY) {
+			resize(input, output, Size(0, 0), scaleX, scaleY, INTER_AREA);
+		}
+		/* Rescale up*/
+		else if (input_height * input_width > scaleX * scaleY) {
+			resize(input, output, Size(0, 0), scaleX, scaleY, INTER_CUBIC);
+		}
+		/* Same size*/
+		else {
+			resize(input, output, Size(0, 0), scaleX, scaleY, INTER_LINEAR);
+		}
+	}
+	else {
+		throw;
+	}
+	return output;
+}
 
