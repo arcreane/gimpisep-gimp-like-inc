c7383e1bcc89160d007652c63a41a076432d2e92
Hugues Baratgin <hugues.baratgin@gmail.com>
2021-06-14 15:43:55 +0200

    The workspace has now a reference to the image saved in memory, modification of the updateImage

--- BetterThanGIMP/src/display/workspace/Workspace.cpp
+++ BetterThanGIMP/src/display/workspace/Workspace.cpp
@@ -7,37 +7,44 @@
 #include <iostream>
 #include "Workspace.h"
 
-Workspace::Workspace(){
+using namespace cv;
+
+Workspace::Workspace(Mat &currentImage) : currentImage(currentImage) {
+    //TEMP
     this->setStyleSheet("QLabel{background-color: red; color: blue;}");
-    this->setText("Hello World!");
+
+    this->setText("You can drag and drop you image here !");
     this->setAcceptDrops(true);
 }
 
-void Workspace::updateImage(cv::Mat image){
-    cv::Mat tmp;
-    cv::cvtColor(image, tmp, cv::COLOR_BGR2RGB);
-    QImage qImage = QImage((uchar*)tmp.data, tmp.cols, tmp.rows, tmp.step, QImage::Format_RGB888)
+void Workspace::updateImageDisplay() {
+    Mat tmp;
+    cvtColor(currentImage, tmp, COLOR_BGR2RGB);
+    QImage qImage = QImage((uchar *) tmp.data, tmp.cols, tmp.rows, tmp.step, QImage::Format_RGB888)
             .scaled(this->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
 
     this->setPixmap(QPixmap::fromImage(qImage));
-    this->workspaceImage = tmp;
-    // Crée une image temporaire à partir du QPixmap affiché, récupérer data, taille etc.
-    // Lui appliquer la transformation
-
-    //this->setScaledContents(true);
-    //this->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
 }
 
-void Workspace::dropEvent(QDropEvent* event){
-    const QMimeData* mimeData = event->mimeData();
+
+void Workspace::updateImageDisplay(Mat image) {
+    Mat tmp;
+    cvtColor(image, tmp, COLOR_BGR2RGB);
+    QImage qImage = QImage((uchar *) tmp.data, tmp.cols, tmp.rows, tmp.step, QImage::Format_RGB888)
+            .scaled(this->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    this->setPixmap(QPixmap::fromImage(qImage));
+}
+
+void Workspace::dropEvent(QDropEvent *event) {
+    const QMimeData *mimeData = event->mimeData();
     std::cout << "Drop event detected" << std::endl;
-    if(mimeData->hasUrls()){
+    if (mimeData->hasUrls()) {
         std::cout << "Success" << std::endl;
-        emit updateWindowImage(mimeData->urls().at(0).toLocalFile());
+        emit onDropEmitFilePath(mimeData->urls().at(0).toLocalFile());
     }
 }
 
 void Workspace::dragEnterEvent(QDragEnterEvent *event) {
     std::cout << "Drag enter event" << std::endl;
     event->accept();
-};
+}
